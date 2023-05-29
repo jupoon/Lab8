2f892664f3983b66617588b4b55139affa07abea
jupoon <130099925+jupoon@users.noreply.github.com>
2023-05-28 19:29:32 -0700

    lab 8 done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Lab 8 - Starter
+
+
+1. Within a GitHub action whenever code is pushed. It's important to continually test as you are writing code and not just once everything is done. Automated test should not be run manually. GitHub actions can do this. 
+2. No
+3. No, I'd use an E2E test. 
+4. Yes. This works best with unit testing.
+
+
+![Image](screenshot_testpass.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,91 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+//PHONE NUMS TESTS//
+test('Dashes and Parenthesis', () => {
+    expect(functions.isPhoneNumber('(123)456-7890')).toBe(true);
+  });
+
+test('No Dashes', () => {
+expect(functions.isPhoneNumber('1234567890')).toBe(false);
+});
+
+test('Too Long', () => {
+    expect(functions.isPhoneNumber('123098409587093784509347823254123-45671234897014293781034298')).toBe(true);
+});
+
+test('Too Short', () => {
+expect(functions.isPhoneNumber('123')).toBe(false);
+});
+
+
+//EMAIL TESTS//
+test('Standard', () => {
+expect(functions.isEmail('jupoon@ucsd.edu')).toBe(true);
+});
+
+test('Strange Characters', () => {
+expect(functions.isEmail('234_789@gmail.com')).toBe(true);
+});
+
+test('Invalid Address', () => {
+expect(functions.isEmail('hi@woah.thisistoolong')).toBe(false);
+});
+
+test('No Email', () => {
+expect(functions.isEmail('clarence')).toBe(false);
+});
+
+
+//PASSWORD TESTS//
+test('Includes Num & Special Character', () => {
+expect(functions.isStrongPassword('P@55word')).toBe(false);
+});
+
+test('No Lowercase', () => {
+expect(functions.isStrongPassword('P@55WORD')).toBe(false);
+});
+
+test('Only Lowercase', () => {
+expect(functions.isStrongPassword('password')).toBe(true);
+});
+
+test('Only Uppercase', () => {
+expect(functions.isStrongPassword('PASSWORD')).toBe(true);
+});
+
+//DATE TESTS//
+test('Short Date', () => {
+expect(functions.isDate('2/2/22')).toBe(false);
+});
+
+test('Long Date', () => {
+expect(functions.isDate('02/02/2022')).toBe(true);
+});
+
+test('Good Date', () => {
+    expect(functions.isDate('02/13/2022')).toBe(true);
+    });
+
+test('Invalid Day/Month/Year', () => {
+expect(functions.isDate('69/69/420')).toBe(false);
+});
+
+
+//HEX TESTS//
+test('Standard', () => {
+    expect(functions.isHexColor('#000000')).toBe(true);
+});
+
+test('Short', () => {
+    expect(functions.isHexColor('000')).toBe(true);
+});
+
+test('Too Long', () => {
+    expect(functions.isHexColor('53535353535353')).toBe(false);
+});
+
+test('Wrong Letter', () => {
+    expect(functions.isHexColor('XYZ')).toBe(false);
+});
